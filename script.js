210a3d7fd695c8cbdad68273306062b4352f2e77
Junechol Ahn <junechol.ahn@gmail.com>
2026-05-03 02:26:25 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+const wrongLettesEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const wordEl = document.getElementById('word');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+let words = [];
+let selectedWord = '';
+
+function resetWord(minlength, maxlength) {
+  // selectedWord = "hangman"
+  // return
+
+  if (words) {
+    selectedWord = '';
+
+    while (selectedWord.length < minlength || selectedWord.length > maxlength) {
+      selectedWord = words[Math.floor(Math.random() * words.length)];
+    }
+  }
+}
+
+async function init() {
+  const res = await fetch('hangman_words.txt');
+  words = (await res.text()).trim().split(',');
+  resetWord(6, 10);
+  console.log(selectedWord);
+  displayWord()
+}
+
+init();
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function restartGame() {
+  correctLetters.length = 0
+  wrongLetters.length = 0
+  resetWord(6,10)
+  updateWrongLettersEl()
+  displayWord()
+  popup.style.display = 'none'
+}
+
+function displayWord() {
+  wordEl.innerHTML = `
+  ${selectedWord.split('').map(letter=>`
+    <span class='letter'>
+      ${correctLetters.includes(letter) ? letter : ""}
+    </span>`
+  ).join('')}`;
+
+  const innerWord = wordEl.innerText.replaceAll("\n", '')
+  console.log(innerWord)
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 😊"
+    popup.style.display = 'flex'
+  }
+}
+
+function updateWrongLettersEl() {
+  wrongLettesEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>': ''}
+    ${wrongLetters.map(letter=>`<span>${letter}</span>`)}
+  `
+
+  figureParts.forEach((part, index)=>{
+    const errors = wrongLetters.length
+
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost. 😭"
+    popup.style.display = 'flex'
+  }
+}
+
+function showNotification() {
+  notification.classList.add('show')
+
+  setTimeout(()=>{
+    notification.classList.remove('show')
+  }, 2000)
+}
+
+playAgainBtn.addEventListener('click', ()=>{
+  restartGame()
+})
+
+
+// keydown letter press
+window.addEventListener('keydown', e => {
+  const isAlphabet = /^[a-z]$/i.test(e.key)
+  if (!isAlphabet) return
+
+  if (correctLetters.includes(e.key) 
+    || wrongLetters.includes(e.key)) {
+    console.log('already used letter')
+    showNotification()
+    return
+  }
+
+  if (selectedWord.includes(e.key)) {
+    correctLetters.push(e.key)
+  } else {
+    wrongLetters.push(e.key)
+    updateWrongLettersEl()
+  }
+  displayWord()
+})
+
